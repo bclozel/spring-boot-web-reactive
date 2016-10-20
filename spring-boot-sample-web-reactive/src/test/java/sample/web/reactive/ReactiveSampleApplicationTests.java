a8bb62088bb415f8701c2e0a80979795287ed7c9
Brian Clozel <bclozel@pivotal.io>
2016-10-20 15:10:18 +0200

    Update tests after WebClient changes

--- spring-boot-sample-web-reactive/src/test/java/sample/web/reactive/ReactiveSampleApplicationTests.java
+++ spring-boot-sample-web-reactive/src/test/java/sample/web/reactive/ReactiveSampleApplicationTests.java
@@ -1,10 +1,5 @@
 package sample.web.reactive;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.web.client.reactive.ClientWebRequestBuilders.*;
-import static org.springframework.web.client.reactive.ResponseExtractors.*;
-import static sample.web.reactive.TestSubscriber.subscribe;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +8,14 @@ import reactor.core.publisher.Mono;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.reactive.ClientRequest;
 import org.springframework.web.client.reactive.WebClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.codec.BodyExtractors.toMono;
+import static sample.web.reactive.TestSubscriber.subscribe;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -29,22 +28,21 @@ public class ReactiveSampleApplicationTests {
 
 	@Before
 	public void setup() {
-		this.webClient = new WebClient(new ReactorClientHttpConnector());
+		this.webClient = WebClient.create(new ReactorClientHttpConnector());
 	}
 
 	@Test
 	public void homeController() {
 
-		Mono<ResponseEntity<BootStarter>> result = this.webClient
-				.perform(get("http://localhost:{port}/", this.port).accept(MediaType.APPLICATION_JSON))
-				.extract(response(BootStarter.class));
+		ClientRequest<Void> request = ClientRequest.GET("http://localhost:{port}/", this.port)
+				.accept(MediaType.APPLICATION_JSON).build();
+
+		Mono<BootStarter> result = this.webClient
+				.exchange(request)
+				.then(response -> response.body(toMono(BootStarter.class)));
 
 		subscribe(result).awaitAndAssertNextValuesWith(
-				response -> {
-					assertThat(response.getStatusCode().value()).isEqualTo(200);
-					assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_UTF8);
-
-					BootStarter starter = response.getBody();
+				starter -> {
 					assertThat(starter.getId()).isEqualTo("spring-boot-starter-web-reactive");
 					assertThat(starter.getLabel()).isEqualTo("Spring Boot Web Reactive");
 				}).assertComplete();
@@ -52,33 +50,32 @@ public class ReactiveSampleApplicationTests {
 
 	@Test
 	public void customArgument() throws Exception {
+		ClientRequest<Void> request = ClientRequest
+				.GET("http://localhost:{port}/custom-arg?content=custom-value", this.port)
+				.accept(MediaType.APPLICATION_JSON).build();
 
-		Mono<ResponseEntity<String>> result = this.webClient
-				.perform(get("http://localhost:{port}/custom-arg?content=custom-value", this.port).accept(MediaType.APPLICATION_JSON))
-				.extract(response(String.class));
+		Mono<String> result = this.webClient
+				.exchange(request)
+				.then(response -> response.body(toMono(String.class)));
 
 		subscribe(result).awaitAndAssertNextValuesWith(
-				response -> {
-					assertThat(response.getStatusCode().value()).isEqualTo(200);
-					assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_UTF8);
-
-					String content = response.getBody();
+				content -> {
 					assertThat(content).contains("custom-value");
 				}).assertComplete();
 	}
 
 	@Test
 	public void staticResources() throws Exception {
-		Mono<ResponseEntity<String>> result = this.webClient
-				.perform(get("http://localhost:{port}/static/spring.txt", this.port).accept(MediaType.TEXT_PLAIN))
-				.extract(response(String.class));
+		ClientRequest<Void> request = ClientRequest
+				.GET("http://localhost:{port}/static/spring.txt", this.port)
+				.accept(MediaType.TEXT_PLAIN).build();
+
+		Mono<String> result = this.webClient
+				.exchange(request)
+				.then(response -> response.body(toMono(String.class)));
 
 		subscribe(result).awaitAndAssertNextValuesWith(
-				response -> {
-					assertThat(response.getStatusCode().value()).isEqualTo(200);
-					assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.TEXT_PLAIN);
-
-					String content = response.getBody();
+				content -> {
 					assertThat(content).contains("Spring Framework");
 				}).assertComplete();
 	}
