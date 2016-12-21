18556d47ad4ece4d40b94cb386046690d063b03d
Brian Clozel <bclozel@pivotal.io>
2016-12-21 14:40:37 +0100

    Update dependencies

--- spring-boot-sample-web-reactive/src/main/java/sample/web/reactive/HomeController.java
+++ spring-boot-sample-web-reactive/src/main/java/sample/web/reactive/HomeController.java
@@ -1,6 +1,7 @@
 package sample.web.reactive;
 
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,13 @@ public class HomeController {
 	@RequestMapping(value = "/")
 	public Mono<BootStarter> starter() {
 		return Mono.just(new BootStarter("spring-boot-starter-web-reactive", "Spring Boot Web Reactive"));
+	}
+
+	@RequestMapping(value = "/starters")
+	public Flux<BootStarter> starters() {
+		return Flux.just(new BootStarter("spring-boot-starter-web-reactive", "Spring Boot Web Reactive"),
+				new BootStarter("spring-boot-starter-web", "Spring Boot Web"),
+				new BootStarter("spring-boot-starter-websocket", "Spring Boot Websocket"));
 	}
 
 	@RequestMapping(value = "/custom-arg")
--- spring-boot-sample-web-reactive/src/test/java/sample/web/reactive/ReactiveSampleApplicationTests.java
+++ spring-boot-sample-web-reactive/src/test/java/sample/web/reactive/ReactiveSampleApplicationTests.java
@@ -3,6 +3,7 @@ package sample.web.reactive;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.subscriber.ScriptedSubscriber;
 
@@ -11,11 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.reactive.ClientRequest;
-import org.springframework.web.client.reactive.WebClient;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.codec.BodyExtractors.toMono;
+import static org.springframework.web.reactive.function.BodyExtractors.toMono;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -39,12 +40,39 @@ public class ReactiveSampleApplicationTests {
 
 		Mono<BootStarter> result = this.webClient
 				.exchange(request)
-				.then(response -> response.body(toMono(BootStarter.class)));
+				.then(response -> response.bodyToMono(BootStarter.class));
 
 		ScriptedSubscriber.<BootStarter>create()
 				.consumeNextWith(starter -> {
 					assertThat(starter.getId()).isEqualTo("spring-boot-starter-web-reactive");
 					assertThat(starter.getLabel()).isEqualTo("Spring Boot Web Reactive");
+				})
+				.expectComplete()
+				.verify(result);
+	}
+
+	@Test
+	public void starters() {
+
+		ClientRequest<Void> request = ClientRequest.GET("http://localhost:{port}/starters", this.port)
+				.accept(MediaType.APPLICATION_JSON).build();
+
+		Flux<BootStarter> result = this.webClient
+				.exchange(request)
+				.flatMap(response -> response.bodyToFlux(BootStarter.class));
+
+		ScriptedSubscriber.<BootStarter>create()
+				.consumeNextWith(starter -> {
+					assertThat(starter.getId()).isEqualTo("spring-boot-starter-web-reactive");
+					assertThat(starter.getLabel()).isEqualTo("Spring Boot Web Reactive");
+				})
+				.consumeNextWith(starter -> {
+					assertThat(starter.getId()).isEqualTo("spring-boot-starter-web");
+					assertThat(starter.getLabel()).isEqualTo("Spring Boot Web");
+				})
+				.consumeNextWith(starter -> {
+					assertThat(starter.getId()).isEqualTo("spring-boot-starter-websocket");
+					assertThat(starter.getLabel()).isEqualTo("Spring Boot Websocket");
 				})
 				.expectComplete()
 				.verify(result);
